ae33054d3866947f6711775ced4460681ea3b5d0
DigaReal <mdfbcosta@gmail.com>
2026-06-16 13:45:57 -0300

    fix: atualiza service worker (v37) para garantir sync de dados do novo caderno

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'minhas-contas-v36';
+﻿const CACHE_NAME = 'minhas-contas-v37';
 const ASSETS = [
   './index.html',
   './css/style-v25.css',
@@ -64,5 +64,6 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
 
 
